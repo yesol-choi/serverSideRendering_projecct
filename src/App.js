dd8e68a3104f6d42b771c973f1744ec9eb875e2b
yesol-choi <ysoli3651@naver.com>
2021-11-02 11:12:45 +0900

    ssr project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage';
+
+
+const App = () => {
+  return (
+    <div>
+      <Menu /> 
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+    </div>
+  )
+}
+
+export default App;
